74e0c9b6d074dad1876bc94e7bf72afee126821f
victor <victor@victors-MacBook-Air.local>
2025-11-19 09:57:40 +0300

    Rewrite bot code with async handler

--- main_bot_webhook.py
+++ main_bot_webhook.py
@@ -1,22 +1,26 @@
-from flask import Flask, request
 from telegram import Bot, Update
-import telegram
-import os
+from flask import Flask, request
 
 TOKEN = "8323792625:AAE-Z7cgncANZOQUlRBCx_qpqkBmJl8GuWM"
-VIDEO_ID = "BAACAgUAAxkBAAIB2Gkcf0DOXbRrzMHBCZKu7KE7mS6hAAIWHwACGh_gVGkJijD4_dr6NgQ"  # твой видео ID
+VIDEO_ID = "BAACAgUAAxkBAAIB2Gkcf0DOXbRrzMHBCZKu7KE7mS6hAAIWHwACGh_gVGkJijD4_dr6NgQ"
 
-bot = Bot(TOKEN)
 app = Flask(__name__)
+bot = Bot(token=TOKEN)
 
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    if update.message:
-        chat_id = update.message.chat.id
-        bot.send_video(chat_id=chat_id, video=VIDEO_ID)
-    return "ok"
+    update = Update.de_json(request.get_json(force=True), bot)
+
+    async def handle():
+        chat_id = update.effective_chat.id
+        await bot.send_video(chat_id=chat_id, video=VIDEO_ID)
+
+    import asyncio
+    asyncio.run(handle())
+    return "OK"
+
 
 if __name__ == "__main__":
+    import os
     PORT = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=PORT)
