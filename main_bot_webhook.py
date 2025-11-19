58326428b29be7b7da05151855917b3cb0700045
victor <victor@victors-MacBook-Air.local>
2025-11-19 12:33:19 +0300

    Update bot with webhook, new video and button

--- main_bot_webhook.py
+++ main_bot_webhook.py
@@ -1,33 +1,36 @@
-import os
 from flask import Flask, request
 from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Dispatcher, CommandHandler
 
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = "8323792625:AAE-Z7cgncANZOQUlRBCx_qpqkBmJl8GuWM"
 VIDEO_ID = "BAACAgUAAxkBAAIB2Gkcf0DOXbRrzMHBCZKu7KE7mS6hAAIWHwACGh_gVGkJijD4_dr6NgQ"
-
 bot = Bot(TOKEN)
-app = Flask(__name__)
 
-@app.route(f"/{TOKEN}", methods=["POST"])
-def webhook():
-    update = Update.de_json(request.get_json(force=True), bot)
-    chat_id = update.message.chat.id
-    message_text = (
+app = Flask(__name__)
+dispatcher = Dispatcher(bot, None, workers=0)
+
+def start(update: Update, context=None):
+    chat_id = update.effective_chat.id
+    text = (
         "Привет! Посмотри это 3х минутное ознакомительное видео, чтобы узнать что такое СТУДИЯ и что от неё ждать 🙂\n\n"
         "СТУДИЯ это онлайн платформа для практики йоги на базе ТЕЛЕГРАМ.\n"
         "В ней удобная навигация по контенту и великолепное качество самих видео.\n"
         "Все тренировки содержат в себе подробные инструкции и пояснения, а названия асан отмечены субтитрами."
     )
-    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Вступить в СТУДИЮ", url="https://t.me/tribute/app?startapp=svnh")]])
-    bot.send_message(chat_id=chat_id, text=message_text)
+    button = InlineKeyboardButton("Вступить в СТУДИЮ", url="https://t.me/tribute/app?startapp=svnh")
+    markup = InlineKeyboardMarkup([[button]])
+    bot.send_message(chat_id=chat_id, text=text, reply_markup=markup)
     bot.send_video(chat_id=chat_id, video=VIDEO_ID)
-    bot.send_message(chat_id=chat_id, text=" ", reply_markup=keyboard)
-    return "OK"
 
-@app.route("/", methods=["GET", "HEAD"])
-def index():
-    return "OK"
+dispatcher.add_handler(CommandHandler("start", start))
+
+@app.route("/webhook", methods=["POST"])
+def webhook_handler():
+    update = Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
+    return "ok"
 
 if __name__ == "__main__":
+    import os
     PORT = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=PORT)
